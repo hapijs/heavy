a43f71c102932114c2e5f4a721a619f0de02d852
Eran Hammer <eran@hammer.io>
2017-09-14 15:42:32 -0700

    v5. Closes #18

--- test/index.js
+++ test/index.js
@@ -24,30 +24,27 @@ describe('Heavy', { parallel: false }, () => {
 
     it('requires load interval when maxEventLoopDelay is set', (done) => {
 
-        const heavy = new Heavy({ sampleInterval: 0 });
         expect(() => {
 
-            heavy.policy({ maxEventLoopDelay: 10, maxHeapUsedBytes: 0, maxRssBytes: 0 });
+            new Heavy({ sampleInterval: 0, maxEventLoopDelay: 10, maxHeapUsedBytes: 0, maxRssBytes: 0 });
         }).to.throw('Load sample interval must be set to enable load limits');
         done();
     });
 
     it('requires load interval when maxHeapUsedBytes is set', (done) => {
 
-        const heavy = new Heavy({ sampleInterval: 0 });
         expect(() => {
 
-            heavy.policy({ maxEventLoopDelay: 0, maxHeapUsedBytes: 10, maxRssBytes: 0 });
+            new Heavy({ sampleInterval: 0, maxEventLoopDelay: 0, maxHeapUsedBytes: 10, maxRssBytes: 0 });
         }).to.throw('Load sample interval must be set to enable load limits');
         done();
     });
 
     it('requires load interval when maxRssBytes is set', (done) => {
 
-        const heavy = new Heavy({ sampleInterval: 0 });
         expect(() => {
 
-            heavy.policy({ maxEventLoopDelay: 0, maxHeapUsedBytes: 0, maxRssBytes: 10 });
+            new Heavy({ sampleInterval: 0, maxEventLoopDelay: 0, maxHeapUsedBytes: 0, maxRssBytes: 10 });
         }).to.throw('Load sample interval must be set to enable load limits');
         done();
     });
@@ -55,7 +52,7 @@ describe('Heavy', { parallel: false }, () => {
     const sleep = function (msec) {
 
         const start = Date.now();
-        while (Date.now() - start < msec) {}
+        while (Date.now() - start < msec) { }
     };
 
     it('measures load', (done) => {
@@ -85,27 +82,25 @@ describe('Heavy', { parallel: false }, () => {
 
     it('throws when process not started', (done) => {
 
-        const heavy = new Heavy({ sampleInterval: 5 });
-        const policy = heavy.policy({ maxRssBytes: 1 });
+        const heavy = new Heavy({ sampleInterval: 5, maxRssBytes: 1 });
 
         expect(() => {
 
-            policy.check();
+            heavy.check();
         }).to.throw('Cannot check load when sampler is not started');
         done();
     });
 
     it('fails check due to high rss load', (done) => {
 
-        const heavy = new Heavy({ sampleInterval: 5 });
-        const policy = heavy.policy({ maxRssBytes: 1 });
+        const heavy = new Heavy({ sampleInterval: 5, maxRssBytes: 1 });
 
         heavy.start();
-        expect(policy.check()).to.equal(null);
+        expect(heavy.check()).to.equal(null);
 
         setTimeout(() => {
 
-            expect(policy.check().message).to.equal('Server under heavy load (rss)');
+            expect(heavy.check().message).to.equal('Server under heavy load (rss)');
             expect(heavy.load.rss).to.be.above(10000);
             heavy.stop();
             done();
@@ -114,15 +109,14 @@ describe('Heavy', { parallel: false }, () => {
 
     it('fails check due to high heap load', (done) => {
 
-        const heavy = new Heavy({ sampleInterval: 5 });
-        const policy = heavy.policy({ maxHeapUsedBytes: 1 });
+        const heavy = new Heavy({ sampleInterval: 5, maxHeapUsedBytes: 1 });
 
         heavy.start();
-        expect(policy.check()).to.equal(null);
+        expect(heavy.check()).to.equal(null);
 
         setTimeout(() => {
 
-            expect(policy.check().message).to.equal('Server under heavy load (heap)');
+            expect(heavy.check().message).to.equal('Server under heavy load (heap)');
             expect(heavy.load.heapUsed).to.be.above(0);
             heavy.stop();
             done();
@@ -131,12 +125,11 @@ describe('Heavy', { parallel: false }, () => {
 
     it('fails check due to high event loop delay load', (done) => {
 
-        const heavy = new Heavy({ sampleInterval: 1 });
-        const policy = heavy.policy({ maxEventLoopDelay: 5 });
+        const heavy = new Heavy({ sampleInterval: 1, maxEventLoopDelay: 5 });
 
         heavy.start();
 
-        expect(policy.check()).to.equal(null);
+        expect(heavy.check()).to.equal(null);
         expect(heavy.load.eventLoopDelay).to.equal(0);
         setImmediate(() => {
 
@@ -144,7 +137,7 @@ describe('Heavy', { parallel: false }, () => {
 
             setImmediate(() => {
 
-                expect(policy.check().message).to.equal('Server under heavy load (event loop)');
+                expect(heavy.check().message).to.equal('Server under heavy load (event loop)');
                 expect(heavy.load.eventLoopDelay).to.be.above(0);
                 heavy.stop();
                 done();
@@ -154,16 +147,15 @@ describe('Heavy', { parallel: false }, () => {
 
     it('fails check due to high event loop delay load (delayed measure)', (done) => {
 
-        const heavy = new Heavy({ sampleInterval: 1 });
-        const policy = heavy.policy({ maxEventLoopDelay: 5 });
+        const heavy = new Heavy({ sampleInterval: 1, maxEventLoopDelay: 5 });
 
         heavy.start();
 
-        expect(policy.check()).to.equal(null);
+        expect(heavy.check()).to.equal(null);
         expect(heavy.load.eventLoopDelay).to.equal(0);
         sleep(10);
 
-        expect(policy.check().message).to.equal('Server under heavy load (event loop)');
+        expect(heavy.check().message).to.equal('Server under heavy load (event loop)');
         expect(heavy.load.eventLoopDelay).to.be.above(0);
         heavy.stop();
         done();
@@ -172,13 +164,12 @@ describe('Heavy', { parallel: false }, () => {
     it('disabled by default', (done) => {
 
         const heavy = new Heavy();
-        const policy = heavy.policy();
 
         heavy.start();
         setImmediate(() => {
 
             expect(heavy.load.rss).to.equal(0);
-            expect(policy.check()).to.equal(null);
+            expect(heavy.check()).to.equal(null);
             heavy.stop();
             done();
         });

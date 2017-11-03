d999c63e024a7d3902a4cb0f2f7d2f0a8ada47e4
Eran Hammer <eran@hammer.io>
2017-11-03 00:42:53 -0700

    Throw in check(). Closes #22

--- test/index.js
+++ test/index.js
@@ -19,30 +19,21 @@ const { describe, it } = exports.lab = Lab.script();
 const expect = Code.expect;
 
 
-describe('Heavy', { parallel: false }, () => {
+describe('Heavy', () => {
 
-    it('requires load interval when maxEventLoopDelay is set', async () => {
+    it('requires load interval when maxEventLoopDelay is set', () => {
 
-        expect(() => {
-
-            new Heavy({ sampleInterval: 0, maxEventLoopDelay: 10, maxHeapUsedBytes: 0, maxRssBytes: 0 });
-        }).to.throw('Load sample interval must be set to enable load limits');
+        expect(() => new Heavy({ sampleInterval: 0, maxEventLoopDelay: 10, maxHeapUsedBytes: 0, maxRssBytes: 0 })).to.throw('Load sample interval must be set to enable load limits');
     });
 
-    it('requires load interval when maxHeapUsedBytes is set', async () => {
+    it('requires load interval when maxHeapUsedBytes is set', () => {
 
-        expect(() => {
-
-            new Heavy({ sampleInterval: 0, maxEventLoopDelay: 0, maxHeapUsedBytes: 10, maxRssBytes: 0 });
-        }).to.throw('Load sample interval must be set to enable load limits');
+        expect(() => new Heavy({ sampleInterval: 0, maxEventLoopDelay: 0, maxHeapUsedBytes: 10, maxRssBytes: 0 })).to.throw('Load sample interval must be set to enable load limits');
     });
 
-    it('requires load interval when maxRssBytes is set', async () => {
+    it('requires load interval when maxRssBytes is set', () => {
 
-        expect(() => {
-
-            new Heavy({ sampleInterval: 0, maxEventLoopDelay: 0, maxHeapUsedBytes: 0, maxRssBytes: 10 });
-        }).to.throw('Load sample interval must be set to enable load limits');
+        expect(() => new Heavy({ sampleInterval: 0, maxEventLoopDelay: 0, maxHeapUsedBytes: 0, maxRssBytes: 10 })).to.throw('Load sample interval must be set to enable load limits');
     });
 
     const sleep = function (msec) {
@@ -71,14 +62,19 @@ describe('Heavy', { parallel: false }, () => {
         heavy.stop();
     });
 
-    it('throws when process not started', async () => {
+    it('ignores load when sample is zero', () => {
+
+        const heavy = new Heavy({ sampleInterval: 0 });
+        heavy.start();
+        heavy.check();
+        heavy.stop();
+    });
+
+    it('throws when process not started', () => {
 
         const heavy = new Heavy({ sampleInterval: 5, maxRssBytes: 1 });
 
-        expect(() => {
-
-            heavy.check();
-        }).to.throw('Cannot check load when sampler is not started');
+        expect(() => heavy.check()).to.throw('Cannot check load when sampler is not started');
     });
 
     it('fails check due to high rss load', async () => {
@@ -86,11 +82,11 @@ describe('Heavy', { parallel: false }, () => {
         const heavy = new Heavy({ sampleInterval: 5, maxRssBytes: 1 });
 
         heavy.start();
-        expect(heavy.check()).to.equal(null);
+        expect(() => heavy.check()).to.not.throw();
 
         await Hoek.wait(10);
 
-        expect(heavy.check().message).to.equal('Server under heavy load (rss)');
+        expect(() => heavy.check()).to.throw('Server under heavy load (rss)');
         expect(heavy.load.rss).to.be.above(10000);
         heavy.stop();
     });
@@ -100,43 +96,26 @@ describe('Heavy', { parallel: false }, () => {
         const heavy = new Heavy({ sampleInterval: 5, maxHeapUsedBytes: 1 });
 
         heavy.start();
-        expect(heavy.check()).to.equal(null);
+        expect(() => heavy.check()).to.not.throw();
 
         await Hoek.wait(10);
 
-        expect(heavy.check().message).to.equal('Server under heavy load (heap)');
+        expect(() => heavy.check()).to.throw('Server under heavy load (heap)');
         expect(heavy.load.heapUsed).to.be.above(0);
         heavy.stop();
     });
 
-    it('fails check due to high event loop delay load', async () => {
-
-        const heavy = new Heavy({ sampleInterval: 1, maxEventLoopDelay: 1 });
-
-        heavy.start();
-
-        expect(heavy.check()).to.equal(null);
-        expect(heavy.load.eventLoopDelay).to.equal(0);
-
-        sleep(10);
-        await Hoek.wait(10);
-
-        expect(heavy.check().message).to.equal('Server under heavy load (event loop)');
-        expect(heavy.load.eventLoopDelay).to.be.above(0);
-        heavy.stop();
-    });
-
-    it('fails check due to high event loop delay load (delayed measure)', async () => {
+    it('fails check due to high event loop delay load (delayed measure)', () => {
 
         const heavy = new Heavy({ sampleInterval: 1, maxEventLoopDelay: 5 });
 
         heavy.start();
 
-        expect(heavy.check()).to.equal(null);
+        expect(() => heavy.check()).to.not.throw();
         expect(heavy.load.eventLoopDelay).to.equal(0);
         sleep(10);
 
-        expect(heavy.check().message).to.equal('Server under heavy load (event loop)');
+        expect(() => heavy.check()).to.throw('Server under heavy load (event loop)');
         expect(heavy.load.eventLoopDelay).to.be.above(0);
         heavy.stop();
     });
@@ -146,11 +125,10 @@ describe('Heavy', { parallel: false }, () => {
         const heavy = new Heavy();
 
         heavy.start();
-        setImmediate(() => {
+        await Hoek.wait(0);
 
-            expect(heavy.load.rss).to.equal(0);
-            expect(heavy.check()).to.equal(null);
-            heavy.stop();
-        });
+        expect(heavy.load.rss).to.equal(0);
+        expect(() => heavy.check()).to.not.throw();
+        heavy.stop();
     });
 });
